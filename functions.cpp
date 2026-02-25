75daa07fa830a7716c2c13a812ffca914f6e2a1a
iabelom <abeletew@gmail.com>
2026-02-25 04:25:55 +0300

    feat: add function overloading example

--- functions.cpp
+++ functions.cpp
@@ -46,4 +46,14 @@ int sum_of_squares(int a, int b) {
 int main() {
     cout << "Sum of squares: " << sum_of_squares(2, 3) << endl;
     return 0;
+}#include <iostream>
+using namespace std;
+
+int sum(int a, int b){ return a+b; }
+double sum(double a, double b){ return a+b; }
+
+int main(){
+    cout << sum(2,3) << endl;
+    cout << sum(2.5,3.5) << endl;
+    return 0;
 }
