0282f100a57d5c4fc0d0494f0cb7e55d301a248a
iabelom <abeletew@gmail.com>
2026-02-25 03:55:40 +0300

    feat: add function with two parameters

--- functions.cpp
+++ functions.cpp
@@ -11,4 +11,15 @@ int main() {
     int sum = add(5, 3);
     cout << "5 + 3 = " << sum << endl;
     return 0;
+}#include <iostream>
+using namespace std;
+
+int multiply(int a, int b) {
+    return a * b;
+}
+
+int main() {
+    int result = multiply(5, 3);
+    cout << "5 * 3 = " << result << endl;
+    return 0;
 }
