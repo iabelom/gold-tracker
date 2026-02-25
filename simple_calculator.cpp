f7fbad5334c1825cfea26a916ef18527f69c40e0
iabelom <abeletew@gmail.com>
2026-02-25 03:45:34 +0300

    feat: add user input to simple calculator

--- simple_calculator.cpp
+++ simple_calculator.cpp
@@ -12,4 +12,20 @@ int main() {
     cout << a << " / " << b << " = " << a/b << endl;
 
     return 0;
+}#include <iostream>
+using namespace std;
+
+// Simple calculator with user input
+
+int main() {
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    cout << a << " + " << b << " = " << a+b << endl;
+    cout << a << " - " << b << " = " << a-b << endl;
+    cout << a << " * " << b << " = " << a*b << endl;
+    cout << a << " / " << b << " = " << a/b << endl;
+
+    return 0;
 }
