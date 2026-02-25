bbe234b278cec61e77bc3b56ec0f01ce8160977b
iabelom <abeletew@gmail.com>
2026-02-25 03:37:19 +0300

    feat: add basic function example

---
+++ functions.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+// Simple function example
+
+void greet(string name) {
+    cout << "Hello, " << name << "!" << endl;
+}
+
+int main() {
+    greet("Iabelom");
+    return 0;
+}
