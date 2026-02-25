01c8425fe9cd839ad6770ce06af743b80a3728dd
iabelom <abeletew@gmail.com>
2026-02-25 03:23:05 +0300

    feat: add variables example in C++

---
+++ variables.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+// Demonstrating variables in C++
+
+int main() {
+    int age = 20;
+    float height = 5.9;
+    char grade = 'A';
+    string name = "Iabelom";
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << endl;
+    cout << "Grade: " << grade << endl;
+
+    return 0;
+}
