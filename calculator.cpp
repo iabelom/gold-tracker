e0212418d3ec89bc9c3b081aa81f8ace69ff227b
iabelom <abeletew@gmail.com>
2026-02-25 04:07:28 +0300

    feat: add loop to continue calculator until exit

--- calculator.cpp
+++ calculator.cpp
@@ -13,4 +13,14 @@ int main() {
 }cout << "3) Multiply 4) Divide\nChoice: ";
 cin >> choice;
 if(choice==3) cout << a*b << endl;
-else if(choice==4) cout << a/b << endl;
+else if(choice==4) cout << a/b << endl;while(true){
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    cout << "1)Add 2)Sub 3)Mul 4)Div 0)Exit: ";
+    cin >> choice;
+    if(choice==0) break;
+    else if(choice==1) cout << a+b << endl;
+    else if(choice==2) cout << a-b << endl;
+    else if(choice==3) cout << a*b << endl;
+    else if(choice==4) cout << a/b << endl;
+}
