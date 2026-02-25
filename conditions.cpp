682ac5d7d55fdae5c0865e3a9d5f1baa2e066c89
iabelom <abeletew@gmail.com>
2026-02-25 03:52:53 +0300

    feat: add even/odd check to conditions example

--- conditions.cpp
+++ conditions.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
 using namespace std;
 
+// User input and conditions
+
+int main() {
+    int number;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    if (number % 2 == 0) {
+        cout << "Number is even" << endl;
+    } else {
+        cout << "Number is odd" << endl;
+    }
+
+    if (number > 0) {
+        cout << "Number is positive" << endl;
+    } else if (number < 0) {
+        cout << "Number is negative" << endl;
+    } else {
+        cout << "Number is zero" << endl;
+    }
+
+    return 0;
+}#include <iostream>
+using namespace std;
+
 // Demonstrating if-else conditions with user input
 
 int main() {
