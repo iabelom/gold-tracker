815978bcbdb4f98654babb8bcce447331419b469
iabelom <abeletew@gmail.com>
2026-02-25 04:03:24 +0300

    feat: add struct array example

--- structs.cpp
+++ structs.cpp
@@ -1,4 +1,6 @@
-#include <iostream>
+git add pointers.cpp
+git commit -m "feat: add pointer to array example"
+git push#include <iostream>
 using namespace std;
 
 // Struct example in C++
@@ -17,4 +19,7 @@ int main() {
     cout << "Age: " << p.age << endl;
 
     return 0;
+}Person people[2] = {{"Alice", 25}, {"Bob", 30}};
+for (int i = 0; i < 2; i++) {
+    cout << "Name: " << people[i].name << ", Age: " << people[i].age << endl;
 }
