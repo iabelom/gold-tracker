9d54882cdd361ad6dcc260ccb0bf331af2198198
iabelom <abeletew@gmail.com>
2026-02-25 04:56:31 +0300

    feat: improve temperature converter menu

--- temperature_converter.cpp
+++ temperature_converter.cpp
@@ -17,4 +17,20 @@ while(cont=='y'){
     cout << "Fahrenheit: " << (c*9/5)+32 << endl;
     cout << "Another conversion? (y/n): ";
     cin >> cont;
-}
+}char choice;
+do{
+    float temp;
+    cout << "Enter 1) Celsius to Fahrenheit 2) Fahrenheit to Celsius: ";
+    cin >> choice;
+    if(choice=='1'){
+        cout << "Celsius: ";
+        cin >> temp;
+        cout << "Fahrenheit: " << (temp*9/5)+32 << endl;
+    } else if(choice=='2'){
+        cout << "Fahrenheit: ";
+        cin >> temp;
+        cout << "Celsius: " << (temp-32)*5/9 << endl;
+    }
+    cout << "Another conversion? (y/n): ";
+    cin >> choice;
+} while(choice=='y');
