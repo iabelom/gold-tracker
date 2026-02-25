ccbaab0973076e7fb172baad9f2f5ae68cfdcbad
iabelom <abeletew@gmail.com>
2026-02-25 03:45:07 +0300

    feat: add simple calculator example

---
+++ simple_calculator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+// Simple calculator
+
+int main() {
+    int a = 5, b = 3;
+
+    cout << a << " + " << b << " = " << a+b << endl;
+    cout << a << " - " << b << " = " << a-b << endl;
+    cout << a << " * " << b << " = " << a*b << endl;
+    cout << a << " / " << b << " = " << a/b << endl;
+
+    return 0;
+}
