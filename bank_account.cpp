64be110c83dbd39e9961719b3501d2d6a14b0a98
iabelom <abeletew@gmail.com>
2026-02-25 04:46:40 +0300

    feat: add deposit and withdraw functions

--- bank_account.cpp
+++ bank_account.cpp
@@ -10,4 +10,6 @@ int main(){
     Account acc1 = {"Iabelom", 1000};
     cout << acc1.name << " balance: " << acc1.balance << endl;
     return 0;
-}
+}git add bank_account.cpp
+git commit -m "feat: add simple bank account project setup"
+git push
