8b5e594bd464ee0b5b84597aead919e6fe4a0da8
iabelom <abeletew@gmail.com>
2026-02-25 03:21:29 +0300

    docs: add detailed comments to hello world program

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+// ===================================
+// Simple Hello World Program
+// Author: Iabelom
+// Purpose: Print greeting to screen
+// ===================================
+
+int main() {
+    cout << "Hello, Iabelom!" << endl;
+    return 0; // End of program
+}
