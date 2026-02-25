7034bd11b6d5280f955a8d34525b17dfdaef56cd
iabelom <abeletew@gmail.com>
2026-02-25 03:39:52 +0300

    feat: add array example in C++

---
+++ arrays.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// Array example in C++
+
+int main() {
+    int numbers[5] = {1, 2, 3, 4, 5};
+
+    for (int i = 0; i < 5; i++) {
+        cout << "Number: " << numbers[i] << endl;
+    }
+
+    return 0;
+}
