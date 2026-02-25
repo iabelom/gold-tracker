3fbb0cce200d5a0afd263eb22bab661774e4a678
iabelom <abeletew@gmail.com>
2026-02-25 04:39:29 +0300

    feat: add win check function for tic-tac-toe

--- tic_tac_toe.cpp
+++ tic_tac_toe.cpp
@@ -34,4 +34,12 @@ for(int i=0;i<3;i++){
         cout << board[i][j] << " ";
     }
     cout << endl;
+}bool checkWin(){
+    for(int i=0;i<3;i++)
+        if(board[i][0]==board[i][1] && board[i][1]==board[i][2]) return true;
+    for(int j=0;j<3;j++)
+        if(board[0][j]==board[1][j] && board[1][j]==board[2][j]) return true;
+    if(board[0][0]==board[1][1] && board[1][1]==board[2][2]) return true;
+    if(board[0][2]==board[1][1] && board[1][1]==board[2][0]) return true;
+    return false;
 }
