3f63610c56b8193ca16d435626b6781bfb569aea
iabelom <abeletew@gmail.com>
2026-02-25 04:04:42 +0300

    feat: add nested struct example

--- structs.cpp
+++ structs.cpp
@@ -22,4 +22,16 @@ int main() {
 }Person people[2] = {{"Alice", 25}, {"Bob", 30}};
 for (int i = 0; i < 2; i++) {
     cout << "Name: " << people[i].name << ", Age: " << people[i].age << endl;
-}
+}struct Date {
+    int day;
+    int month;
+    int year;
+};
+
+struct Event {
+    string name;
+    Date date;
+};
+
+Event e = {"Birthday", {25,12,2026}};
+cout << e.name << " on " << e.date.day << "/" << e.date.month << "/" << e.date.year << endl;
