df9f9ba93d7b460effb9c57920e753d1f242475e
iabelom <abeletew@gmail.com>
2026-02-25 04:06:54 +0300

    feat: add multiplication/division to calculator

--- calculator.cpp
+++ calculator.cpp
@@ -10,4 +10,7 @@ int main() {
     if(choice==1) cout << a+b << endl;
     else cout << a-b << endl;
     return 0;
-}
+}cout << "3) Multiply 4) Divide\nChoice: ";
+cin >> choice;
+if(choice==3) cout << a*b << endl;
+else if(choice==4) cout << a/b << endl;
