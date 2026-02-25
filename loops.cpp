8832b5595a46911bd9a3bb6f983899fea70e6aca
iabelom <abeletew@gmail.com>
2026-02-25 03:36:10 +0300

    feat: add while loop example

--- loops.cpp
+++ loops.cpp
@@ -11,4 +11,26 @@ int main() {
     }
 
     return 0;
+}#include <iostream>
+using namespace std;
+
+// Demonstrating loops in C++
+
+int main() {
+
+    // For loop example
+    for (int i = 1; i <= 5; i++) {
+        cout << "For Loop Count: " << i << endl;
+    }
+
+    cout << "------" << endl;
+
+    // While loop example
+    int j = 1;
+    while (j <= 5) {
+        cout << "While Loop Count: " << j << endl;
+        j++;
+    }
+
+    return 0;
 }
