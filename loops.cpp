e26cdff3adc3335dad99424ee75661a65b8044d3
iabelom <abeletew@gmail.com>
2026-02-25 03:34:49 +0300

    feat: add for loop example in C++

---
+++ loops.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// Demonstrating loops in C++
+
+int main() {
+
+    // For loop example
+    for (int i = 1; i <= 5; i++) {
+        cout << "Count: " << i << endl;
+    }
+
+    return 0;
+}
